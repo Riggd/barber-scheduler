74f9528db0c338f3c7a224e3098be959e50d6d03
Derek Onay <derek@wr3tched@gmail.com>
2016-02-14 16:49:32 -0500

    Project Structure

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php // DEREK   [ https://web-project-ryanwiesjahn.c9users.io/drock/ ]
+    echo "php is working Good job!";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,13 +9,12 @@
     <title>Angular Scheduling</title>
 
     <!-- CSS -->
-    <!-- load Bootstrap and custom style.css -->
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootswatch/3.3.0/journal/bootstrap.min.css">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.0/journal/bootstrap.min.css">
+    <link href="../template/css/_compiled/styles.css" rel="stylesheet">
+    <link rel="stylesheet" href="scss/_compiled/styles.css">
 
     <!-- JS -->
-    <!-- load Angular, Firebase, Firebase Angular Library, and custom app.js -->
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.0/angular.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.0/angular.min.js"></script>
     <script src="https://cdn.firebase.com/js/client/1.1.1/firebase.js"></script>
     <script src="https://cdn.firebase.com/libs/angularfire/0.8.0/angularfire.min.js"></script>
     <script src="app.js"></script>
@@ -23,12 +26,11 @@
     <h1>Promised Excellence Barbershop</h1>
   </div>
 
-  <!-- were doing one day with 1 time slot for simplicity -->
-  <!-- we'll use angular's ng-repeat in a bit to get the other days and times -->
+
   <div class="row times">
     <div class="col-xs-4 text-center" ng-repeat="day in days">
       <h2>{{day.name}}</h2>
-      <div class="time-slot" ng-repeat="slot in day.slots">
+      <div class="post-card" ng-repeat="slot in day.slots">
           <input type="checkbox" id="{{day.name}}-{{$index}}" ng-model="slot.booked" ng-disabled="slot.booked">
           <label for="{{day.name}}-{{$index}}">{{slot.time}}<br>
             <span ng-if="slot.booked">Booked</span>
